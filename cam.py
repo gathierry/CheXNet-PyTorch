c4dc6d258ad97c0590f7e8af1f49402b3a7f1d0a
Shiyu <ligary04@hotmail.com>
2018-09-30 13:43:24 +0800

    cam

--- cam.py
+++ cam.py
@@ -1,22 +1,19 @@
 import numpy as np
 import torch
-from torch.autograd import Variable
-from torch.backends import cudnn
 import cv2
-from torch.nn import DataParallel
-from torch.utils.data import DataLoader
 
-from data_generator import DataGenerator
-import pytorch_utils
-from config import Config
-from densenet import DenseNet121
-
-
-def cam(fm, w):
-    fm = fm[0]
-    M = torch.mm(w, fm.view(fm.size(0), -1))
-    M = M.view(M.size(0), fm.size(1), fm.size(2)).data.cpu().numpy()
-    return M
+def cam(fms, w):
+    """
+    :param fms: tensor [n, 1024, 7, 7]
+    :param w: tensor [1, 1024]
+    :return: a list of feature maps
+    """
+    Ms = []
+    for fm in fms:
+        M = torch.mm(w, fm.view(fm.size(0), -1))
+        M = M.view(M.size(0), fm.size(1), fm.size(2)).data.cpu().numpy()
+        Ms.append(M)
+    return Ms
 
 def draw_heatmap(image, heatmap):
     alpha = 0.5
